03d69cac66fa90aee8956dcd3063608458e8554b
defenstration <41651534+defenstration@users.noreply.github.com>
2023-04-05 12:02:55 -0400

    Add files via upload

--- Data Entry Form.py
+++ Data Entry Form.py
@@ -8,7 +8,11 @@ Created on Sun Mar 12 12:50:43 2023
 
 import PySimpleGUI as sg
 import pandas as pd
+import datetime
 
+# Creating Timestamp
+now = datetime.datetime.now()
+t = now.strftime("%m/%d/%Y, %H:%M:%S")
 
 EXCEL_FILE = 'Timekeeping.xlsx'
 df = pd.read_excel(EXCEL_FILE)
@@ -22,6 +26,9 @@ layout = [
     [sg.Text('ID', size = (4,1)), sg.InputText(key='ID')],
     [sg.Text('Product', size=(15,1)), sg.Combo(['Puzzles', 'Banks', 'Chalkboards', 'Tic Tac Toe','Post it Holder'], key='Product')],
     [sg.Text('Operation', size= (15,1)), sg.Combo(['Router', 'Laser', 'Print'], key = "Operation")],
+    [sg.Checkbox('Start', size = (5,1), key = 'Start')], 
+    [sg.Checkbox('End', size = (3,1), key = 'End')],
+    [sg.Text('Date:', size = (4,1)), sg.Input(t, key = 'Date')],
     [sg.Submit(), sg.Button('Clear'), sg.Exit()]
     ]
 
@@ -43,6 +50,7 @@ while True:
         clear_input()
     
     if event == 'Submit':
+        
         df = df.append(values, ignore_index = True)
         df.to_excel(EXCEL_FILE, index = False)
         sg.popup('Data Saved!')
